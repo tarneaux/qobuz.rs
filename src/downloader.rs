a1adf38aaada97f4f2ddc15304ddb358174ffe76
tarneo <tarneo@tarneo.fr>
2025-01-13 13:52:39 +0100

    downloader: Split download_and_tag_track into multiple methods

--- src/downloader.rs
+++ src/downloader.rs
@@ -32,10 +32,30 @@ impl Downloader {
         E1: Sync,
         E2: Sync,
     {
-        let album_loc = get_standard_album_location(&self.root, album);
-        if !album_loc.is_dir() {
-            std::fs::create_dir_all(&album_loc)?;
-        }
+        let track_loc = self.download_track(track, album, quality, force).await?;
+        let cover_raw = reqwest::get(album.image.large.clone())
+            .await?
+            .bytes()
+            .await?;
+        let cover = audiotags::Picture::new(&cover_raw, audiotags::MimeType::Jpeg);
+        tag_track(track, &track_loc, album, cover)?;
+        Ok(track_loc)
+    }
+
+    async fn download_track<E1, E2>(
+        &self,
+        track: &Track<E1>,
+        album: &Album<E2>,
+        quality: Quality,
+        force: bool,
+    ) -> Result<PathBuf, DownloadError>
+    where
+        Track<E1>: Extra + Send,
+        Album<E2>: Extra + Send,
+        E1: Sync,
+        E2: Sync,
+    {
+        self.ensure_album_dir_exists(album)?;
         let track_loc = get_standard_track_location(&self.root, track, album, &quality);
         let mut out = match OpenOptions::new()
             .write(true)
@@ -58,13 +78,18 @@ impl Downloader {
         while let Some(item) = bytes_stream.next().await {
             tokio::io::copy(&mut item?.as_ref(), &mut out).await?;
         }
-        let cover_raw = reqwest::get(album.image.large.clone())
-            .await?
-            .bytes()
-            .await?;
-        let cover = audiotags::Picture::new(&cover_raw, audiotags::MimeType::Jpeg);
-        tag_track(track, &track_loc, album, cover)?;
         Ok(track_loc)
+    }
+
+    fn ensure_album_dir_exists<E>(&self, album: &Album<E>) -> Result<(), std::io::Error>
+    where
+        Album<E>: Extra,
+    {
+        let album_loc = get_standard_album_location(&self.root, album);
+        if !album_loc.is_dir() {
+            std::fs::create_dir_all(&album_loc)?;
+        }
+        Ok(())
     }
 }
 
